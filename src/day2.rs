954e2208759c58f57cbe71f044d364c3b0d0b689
PhroZen <mail@mikael-berglund.se>
2023-12-12 21:02:18 +0100

    Day 2 TWO star.

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::fs;
 
 #[derive(PartialEq, Debug)]
@@ -7,12 +8,19 @@ pub struct Limit {
     pub(crate) b: u32,
 }
 
-pub fn parse_file(path: &str, limit: &Limit) -> u32 {
+pub fn parse_file_part_1(path: &str, limit: &Limit) -> u32 {
     read_lines_from_file(path)
         .lines()
-        .map(|row| parse_and_validate(row, limit))
+        .map(|row| parse_and_validate_day_1(row, limit))
         .filter(|(b, _)| *b)
         .map(|(_, game_id)| game_id)
+        .sum()
+}
+
+pub fn parse_file_part_2(path: &str) -> u32 {
+    read_lines_from_file(path)
+        .lines()
+        .map(|row| parse_day_2(row))
         .sum()
 }
 
@@ -21,7 +29,7 @@ fn read_lines_from_file(path: &str) -> String {
         .expect(format!("{path} file should be in place").as_str())
 }
 
-fn parse_and_validate(row: &str, limit: &Limit) -> (bool, u32) {
+fn parse_and_validate_day_1(row: &str, limit: &Limit) -> (bool, u32) {
     let mut game: u32 = 0;
     let mut r: u32 = 0;
     let mut g: u32 = 0;
@@ -90,6 +98,57 @@ fn parse_and_validate(row: &str, limit: &Limit) -> (bool, u32) {
     return (true, game);
 }
 
+
+fn parse_day_2(row: &str) -> u32 {
+    let mut r_max: u32 = 0;
+    let mut g_max: u32 = 0;
+    let mut b_max: u32 = 0;
+    let mut temp: u32 = 0;
+    let mut i = "Game ".len();
+    let mut found_colon = false;
+    let chars = row.as_bytes(); // assuming ASCII
+    while i < row.len() {
+        let mut current_char = chars[i] as char;
+        while !found_colon {
+            found_colon = current_char == ':';
+            i += 1;
+            if found_colon {
+                i += 1;
+            }
+            current_char = chars[i] as char;
+        }
+
+        if current_char == ';' {
+            i += 2;
+            current_char = chars[i] as char;
+        }
+
+        if current_char.is_numeric() {
+            let (steps, number) = read_complete_number(chars, i);
+            i += steps + 1;
+            current_char = chars[i] as char;
+            temp = number;
+        }
+
+        if current_char == 'g' {
+            g_max = max(g_max, temp);
+            i += "green".len();
+            continue;
+        } else if current_char == 'b' {
+            b_max = max(b_max, temp);
+            i += "blue".len();
+            continue;
+        } else if current_char == 'r' {
+            r_max = max(r_max, temp);
+            i += "red".len();
+            continue;
+        }
+        i += 1;
+    }
+    return r_max * g_max * b_max;
+}
+
+
 fn read_complete_number(chars: &[u8], i: usize) -> (usize, u32) {
     let mut j: usize = 0;
     let mut s: String = String::from("");
@@ -107,7 +166,7 @@ mod tests {
 
     #[test]
     fn basic_parse() {
-        let (passed, game_id) = parse_and_validate(
+        let (passed, game_id) = parse_and_validate_day_1(
             "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
             &Limit { r: 5, g: 4, b: 9 });
         assert_eq!(game_id, 1);
@@ -116,11 +175,16 @@ mod tests {
 
     #[test]
     fn multi_numbers_parse() {
-        let (passed, game_id) = parse_and_validate(
+        let (passed, game_id) = parse_and_validate_day_1(
             "Game 132: 12 blue, 213 red; 1 red, 1 green, 1 blue; 3 green",
             &Limit { r: 5, g: 4, b: 9 });
         assert_eq!(game_id, 132);
         assert_eq!(passed, false);
     }
-}
 
+    #[test]
+    fn star() {
+        let x = parse_day_2("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red");
+        println!("{}", x);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ mod day2;
 fn main() {
     let start = Instant::now();
     println!("day1: {}", day1::parse_file("./data/day1/day_1_input.dat"));
+    println!("day2: {}", day2::parse_file_part_1("./data/day2/day2.dat",&Limit { r: 12, g: 13, b: 14 }));
+    println!("day2p2: {}", day2::parse_file_part_2("./data/day2/day2.dat"));
     let duration = start.elapsed();
     println!("Time elapsed in expensive_function() is: {:?}", duration);
-    println!("day1: {}", day2::parse_file("./data/day2/day2.dat",
-                                          &Limit { r: 12, g: 13, b: 14 }));
 }
